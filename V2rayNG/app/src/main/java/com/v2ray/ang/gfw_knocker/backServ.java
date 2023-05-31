c6a890af372efe288f59a04606ec7e3b47b40404
GFW-knocker <123458121+GFW-knocker@users.noreply.github.com>
2023-05-31 11:02:01 +0330

    add fragmentor java files

---
+++ V2rayNG/app/src/main/java/com/v2ray/ang/gfw_knocker/backServ.java
@@ -0,0 +1,47 @@
+package com.v2ray.ang.gfw_knocker;
+
+public class backServ {
+    DoH_over_Fragment DoH_service;
+    HTTPS_Fragmentor HTTPS_serv;
+    TLS_Fragmentor TLS_serv;
+
+    // constructor
+    public backServ(){
+
+        // DoH service to be used inside code for DNS
+        DoH_service = new DoH_over_Fragment("google",
+                "8.8.8.8", 443,
+                true, 87, 0.001);
+
+
+        // HTTPS proxy listening on 127.0.0.1:4500
+        HTTPS_serv = new HTTPS_Fragmentor("127.0.0.1",4500,
+                null, -1,
+                DoH_service,true,
+                87,0.001);
+
+    }
+
+    public void start_HTTPS_service(){
+        HTTPS_serv.start();
+    }
+
+    public void stop_HTTPS_service(){
+        HTTPS_serv.safely_stop_server();
+        DoH_service.safely_stop_DoH();
+    }
+
+
+    public void start_TLS_service(){
+        TLS_serv = new TLS_Fragmentor("127.0.0.1",2500 ,
+                "discord.com",443,true,
+                87,0.001 );
+        TLS_serv.start();
+
+    }
+
+    public void stop_TLS_service(){
+        TLS_serv.safely_stop_server();
+    }
+
+}
